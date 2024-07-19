890f82a1e395eff034782c06ebffa1be4b1172dc
Isham17NIT <ishamahuja17@gmail.com>
2024-07-19 14:27:22 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,67 @@
+// Task1
+var x=17;
+console.log(x);
+
+// Task 2
+let Name="Isham";
+console.log(Name);
+
+// Task 3
+const b1=true;
+console.log(b1);
+
+// Task4
+let num=4;
+let str="OCD";
+let b2=false;
+const college = {
+    Name:"NIT KKR",
+    branch:"CS",
+    year:2
+};
+const fruits=["apple","mango","kiwi","litchi"];
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof b2);
+console.log(typeof college);
+console.log(typeof fruits);
+
+//Task 5
+let t=4;
+console.log(t);
+t=8;
+console.log(t);
+
+//Task 6
+// const a=2;
+// a=3;
+// console.log(a);
+// //Output->TypeError: Assignment to constant variable.
+
+// Feature Request 1
+let n=20;
+let myName="Isham"
+let booleanValue=true;
+const person = {
+    Name:"John",
+    gender:"male",
+    age:30,
+    city:"Delhi"    
+};
+const branches=["CS","IT","M&C","AIML"];
+console.log(n,typeof n);
+console.log(myName,typeof myName);
+console.log(booleanValue,typeof booleanValue);
+console.log(person,typeof person);
+console.log(branches,typeof branches);
+
+// Feature Request 2
+let var1 = "I can be reassigned any value";
+console.log(var1);
+var1 = "See! I've been reassigned another value";
+console.log(var1);
+
+const var2 = "My value is fixed";
+console.log(var2);
+// Below line will cause an error
+//var2 = "Trying to change my value..";
