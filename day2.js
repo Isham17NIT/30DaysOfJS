0dc36d2e0f3d139f335a36846b57c5c6929c024a
Isham17NIT <ishamahuja17@gmail.com>
2024-07-20 08:21:03 +0530

    added day2.js

---
+++ day2.js
@@ -0,0 +1,80 @@
+// Task 1
+let n1=5;
+let n2=6;
+console.log(n1+n2); // 11
+
+// Task 2
+let n3=19;
+let n4=11;
+console.log(n3-n4); // 8
+
+//Task 3
+let n5=15;
+let n6=8;
+console.log(n5*n6); // 120
+
+//Task 4
+let n7=75;
+let n8=4;
+console.log(n7/n8); // 18.75
+
+//Task 5
+let n9=75;
+let n10=4;
+console.log(n9%n10); // 3
+
+//Task 6
+let x=9;
+x+=10;
+console.log(x); // 19
+
+//Task 7
+let y=34;
+y-=12;
+console.log(y); // 22
+
+//Task 8
+console.log(34>12); // true
+console.log(34<12); // false
+
+//Task 9
+console.log(17>=17); // true
+console.log(17<=12); // false
+
+//Task 10
+let x1=12;
+let x2="12";
+console.log(x1==x2);  // true
+console.log(x1===x2); // false
+
+//Task 11
+let i=56;
+while(i>0 && i%2==0)
+{
+    i=i/2;
+}
+console.log(i); // 7
+
+//Task 12
+let age=34;
+if(age==18 || age>18)
+{
+    console.log("eligible to vote") // this will be printed
+}
+else
+{
+    console.log("not eligible to vote");
+}
+
+//Task 13
+if(!false)
+{
+    console.log("stmt executed");
+}
+
+//Task 14
+let num=-7;
+console.log(num>=0?"positive":"negative");
+
+
+
